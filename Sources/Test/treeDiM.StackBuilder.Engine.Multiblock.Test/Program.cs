a336f09c95bc653604c1c6970627048452e58d4d
fgasnier71 <fgasnier71@gmail.com>
2021-08-12 23:20:12 +0200

    Added multiblock analysis class...

---
+++ Sources/Test/treeDiM.StackBuilder.Engine.Multiblock.Test/Program.cs
@@ -0,0 +1,39 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sharp3D.Math.Core;
+
+using log4net;
+using log4net.Config;
+using treeDiM.StackBuilder.Basics;
+using treeDiM.StackBuilder.Engine;
+using treeDiM.StackBuilder.Graphics;
+#endregion
+
+namespace treeDiM.StackBuilder.Engine.Multiblock.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            _log.Info("Started...");
+
+            // instantiate pallet
+            var dimContainer = new Vector3D(1200.0, 1000.0, 1700.0);
+            var dimCase = new Vector3D(400.0, 350.0, 100.0);
+
+            var multiblockSolutions = 
+
+
+            _log.Info("Done...");
+        }
+
+        protected static ILog _log = LogManager.GetLogger(typeof(Program));
+    }
+}
---
+++ Sources/TreeDim.StackBuilder.Basics/Solutions/SolutionMultiBlock.cs
@@ -0,0 +1,48 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using treeDiM.Basics;
+#endregion
+
+namespace treeDiM.StackBuilder.Basics.Solutions
+{
+    public class AnalysisMultiBlock
+    {
+        public PackableBrickNamed Content { set; get; }
+        public double ContainerLoadingVolume { get; }
+    }
+
+    internal class Block
+    {
+        public int ItemCount { get; }
+        public BBox3D BBox
+        {
+            get;
+        }
+        
+    }
+    public class SolutionMultiBlock
+    {
+        public BBox3D BBoxLoadWDeco
+        {
+            get
+            {
+                var bbox = BBox3D.Initial;
+                return bbox;
+            }
+        }
+        public BBox3D BBoxGlobal => BBox3D.Initial;
+
+        public OptDouble NetWeight => ItemCount * Analysis.Content.NetWeight;
+        public double VolumeEfficiency => 100.0;
+
+        public int ItemCount => BlockList.Sum(b => b.ItemCount);
+
+        private List<Block> BlockList { get; set; } = new List<Block>();
+        private AnalysisMultiBlock Analysis { get; set; }
+    }
+}
