a05320b7bcd5bf37fc4248b1cf67b7976b4e473f
fgasnier71 <fgasnier71@gmail.com>
2022-01-01 18:39:20 +0100

    Created class Block+BlockBuilder...

---
+++ Sources/TreeDim.StackBuilder.Engine/Solvers/BlockBuilder.cs
@@ -0,0 +1,23 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sharp3D.Math.Core;
+
+using treeDiM.StackBuilder.Basics;
+#endregion
+
+namespace treeDiM.StackBuilder.Engine.Solvers
+{
+    public class BlockBuilder
+    {
+        public static List<Block2D> Build(Vector2D itemDimension, Vector2D containerDim, int number)
+        {
+            var list = new List<Block2D>();
+            return list;
+        }
+    }
+}
